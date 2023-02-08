3dff330a6828911b3e4ac3920cbc75958a9afe4f
michael-gokey-architect <michael.gokey.architect@gmail.com>
2023-02-08 18:19:25 -0500

    removed a chunk of samples

--- 07-currency-exchange-microservice-stackdriver/src/main/java/com/in28minutes/microservices/currencyexchangeservice/resource/ExchangeValueRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mfe.microservices.currencyexchangeservice.resource;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
-	ExchangeValue findByFromAndTo(String from, String to);
-}
